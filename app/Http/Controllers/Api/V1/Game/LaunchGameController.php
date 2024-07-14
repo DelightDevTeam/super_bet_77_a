1290f98642c9877b393b85b23427f6636b6550fc
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-07-14 08:55:16 +0630

    launch game

--- app/Http/Controllers/Api/V1/Game/LaunchGameController.php
+++ app/Http/Controllers/Api/V1/Game/LaunchGameController.php
@@ -66,54 +66,6 @@ class LaunchGameController extends Controller
             return response()->json(['error' => 'An unexpected error occurred', 'exception' => $e->getMessage()], 500);
         }
     }
+
     
-    public function directlaunchGame(Request $request)
-    {
-        $validatedData = $request->validate([
-            'productId' => 'required|integer',
-            'gameType' => 'required|integer',
-        ]);
-
-        // Retrieve user and configuration settings
-        $user = Auth::user();
-        $operatorCode = Config::get('game.api.operator_code');
-        $secretKey = Config::get('game.api.secret_key');
-        $apiUrl = Config::get('game.api.url').'/Seamless/LaunchGame';
-        $password = Config::get('game.api.password');
-        // Generate the signature
-        $requestTime = now()->format('YmdHis');
-        $signature = md5($operatorCode.$requestTime.'launchgame'.$secretKey);
-
-        // Prepare the payload
-        $data = [
-            'OperatorCode' => $operatorCode,
-            'MemberName' => $user->user_name, // Assume username is the member identifier
-            'DisplayName' => $validatedData['displayName'] ?? $user->name,
-            'Password' => $password,
-            'ProductID' => $validatedData['productId'],
-            'GameType' => $validatedData['gameType'],
-            'GameID' => $validatedData['gameId'],
-            'LanguageCode' => self::ENG_LANGUAGE_CODE,
-            'Platform' => self::WEB_PLAT_FORM,
-            'IPAddress' => $request->ip(),
-            'Sign' => $signature,
-            'RequestTime' => $requestTime,
-        ];
-        try {
-            // Send the request
-            $response = Http::withHeaders([
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-            ])->post($apiUrl, $data);
-
-            if ($response->successful()) {
-                return $response->json();
-            }
-
-            return response()->json(['error' => 'API request failed', 'details' => $response->body()], $response->status());
-        } catch (\Throwable $e) {
-
-            return response()->json(['error' => 'An unexpected error occurred', 'exception' => $e->getMessage()], 500);
-        }
-    }
 }
