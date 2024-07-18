37f4aac9a8eeffed00fa191dc9c49dbed0eea35b
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-07-18 11:57:19 +0700

    a

--- app/Http/Resources/HistoryResource.php
+++ app/Http/Resources/HistoryResource.php
@@ -15,14 +15,15 @@ class HistoryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "id" => $this->id,
-            "payment_type" => $this->payment_type->name,
-            "account_name" => $this->account_name,
-            "account_no" => $this->account_no,
-            "amount" => $this->amount,
-            "status" => $this->status === 0 ? "Pending" : $this->status === 1 ? "Success" : "Reject",
-            "message" => $this->message,    
-            "datetime" => $this->created_at->format("Y-m-d H:i:s")
+            "id" => $this->id,  // Gets the 'id' property of the object
+            "name" => $this->user->name,  // Gets the 'name' property from the related 'user' object
+            "payment_type" => $this->paymentType->name,  // Gets the 'name' property from the related 'paymentType' object
+            "account_name" => $this->account_name,  // Gets the 'account_name' property of the object
+            "account_no" => $this->account_no,  // Gets the 'account_no' property of the object
+            "amount" => $this->amount,  // Gets the 'amount' property of the object
+            "status" => $this->status === 0 ? "Pending" : ($this->status === 1 ? "Success" : "Reject"),  // Converts the 'status' property to a human-readable string
+            "message" => $this->message,  // Gets the 'message' property of the object
+            "datetime" => $this->created_at->format("Y-m-d H:i:s")  // Formats the 'created_at' property as a string
         ];
     }
 }
