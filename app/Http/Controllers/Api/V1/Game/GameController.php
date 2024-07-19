a4b905628f8fa377b10d60bd55e42b2d75b60951
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-07-19 10:35:51 +0700

    game list resource fixed

--- app/Http/Controllers/Api/V1/Game/GameController.php
+++ app/Http/Controllers/Api/V1/Game/GameController.php
@@ -65,10 +65,11 @@ class GameController extends Controller
 
     public function gameList($product_id, $game_type_id)
     {
-        $gameLists = GameList::where('product_id', $product_id)
+        $gameLists = GameList::with('product')->where('product_id', $product_id)
             ->where('game_type_id', $game_type_id)->get();
 
         return $this->success(GameDetailResource::collection($gameLists), 'Game Detail Successfully');
+
     }
 
     public function getGameDetail($provider_id, $game_type_id)
