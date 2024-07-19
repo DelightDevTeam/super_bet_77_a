453384c322fc01a6ebf0678d91b73baa974655f1
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-07-19 10:38:36 +0700

    a

--- app/Http/Controllers/Api/V1/Game/GameController.php
+++ app/Http/Controllers/Api/V1/Game/GameController.php
@@ -65,8 +65,11 @@ class GameController extends Controller
 
     public function gameList($product_id, $game_type_id)
     {
-        $gameLists = GameList::with('product')->where('product_id', $product_id)
-            ->where('game_type_id', $game_type_id)->get();
+        $gameLists = GameList::with('product')
+        ->where('product_id', $product_id)
+        ->where('game_type_id', $game_type_id)
+        ->where('status', 1)
+        ->get();
 
         return $this->success(GameDetailResource::collection($gameLists), 'Game Detail Successfully');
 
