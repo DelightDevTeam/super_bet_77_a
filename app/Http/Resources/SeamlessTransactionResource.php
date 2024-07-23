77d5ec354ccd7c2d85bf6a0d97b38ddb8a5e4cfd
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-07-23 07:28:07 +0700

    a

--- app/Http/Resources/SeamlessTransactionResource.php
+++ app/Http/Resources/SeamlessTransactionResource.php
@@ -15,7 +15,6 @@ class SeamlessTransactionResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "id" => $this->id,
             "from_date" => $this->from_date,
             "to_date" => $this->to_date,
             "product" => $this->product_name,
