7c545b11dfbfb22d6a0210aad27016f16504ce48
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-07-24 10:34:01 +0630

    game list updated

--- resources/views/admin/game_list/paginate_index.blade.php
+++ resources/views/admin/game_list/paginate_index.blade.php
@@ -1,4 +1,21 @@
 @extends('admin_layouts.app')
+
+@section('styles')
+<style>
+.transparent-btn {
+    background: none;
+    border: none;
+    padding: 0;
+    outline: none;
+    cursor: pointer;
+    box-shadow: none;
+    appearance: none; /* For some browsers */
+}
+</style>
+<!-- DataTables CSS -->
+<link href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+@endsection
+
 @section('content')
 <div class="row mt-4">
     <div class="col-12">
@@ -10,10 +27,16 @@
                         <h5 class="mb-0">Game List Dashboards
                             <span>
                                 <p>
-                                    All Total Running Games on Site: {{ $games }}
+                                    All Total Running Games on Site: {{ count($games) }}
                                 </p>
                             </span>
                         </h5>
+                    </div>
+                    <div class="ms-auto my-auto mt-lg-0 mt-4">
+                        <div class="ms-auto my-auto">
+                            {{-- <a href="" class="btn bg-gradient-primary btn-sm mb-0 py-2">+&nbsp; Create New User</a> --}}
+                            {{-- <button class="btn btn-outline-primary btn-sm export mb-0 mt-sm-0 mt-1" data-type="csv" type="button" name="button">Export</button> --}}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -22,6 +45,7 @@
                 <table class="table table-flush" id="users-search">
                     <thead>
                         <tr>
+                            <th>#</th>
                             <th class="bg-success text-white">Game Type</th>
                             <th class="bg-danger text-white">Product</th>
                             <th class="bg-info text-white">Game Name</th>
@@ -43,10 +67,10 @@
 
 @section('scripts')
 <!-- jQuery -->
-<script src="{{asset('admin_app/assets/js/jquery.min.js')}}"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- DataTables JS -->
-<script src="{{asset('admin_app/assets/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('admin_app/assets/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
 <script>
 $(document).ready(function() {
     $('#users-search').DataTable({
@@ -54,6 +78,7 @@ $(document).ready(function() {
         serverSide: true,
         ajax: "{{ route('admin.gameLists.index') }}",
         columns: [
+            {data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false},
             {data: 'game_type', name: 'gameType.name'},
             {data: 'product', name: 'product.name'},
             {data: 'name', name: 'name'},
@@ -74,4 +99,6 @@ $(document).ready(function() {
     });
 });
 </script>
+
+
 @endsection
