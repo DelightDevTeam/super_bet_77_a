e4c94bcd89df0f681b1085b86769e950b6fa7d51
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-07-31 08:18:29 +0630

    route

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,7 @@ Route::post('/logout', [AuthController::class, 'logout']);
 Route::get('promotion', [PromotionController::class, 'index']);
 Route::get('banner', [BannerController::class, 'index']);
 Route::get('bannerText', [BannerController::class, 'bannerText']);
+Route::get('adspopup', [BannerController::class, 'AdsBannerIndex']);
 Route::get('v1/validate', [AuthController::class, 'callback']);
 Route::get('gameTypeProducts/{id}', [GameController::class, 'gameTypeProducts']);
 Route::get('allGameProducts', [GameController::class, 'allGameProducts']);
