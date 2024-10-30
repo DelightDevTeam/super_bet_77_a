84586d3de54211840757283bbc02387881fe9ad1
AMK1215 <aungmyokhaing1215@gmail.com>
2024-10-30 16:49:01 +0630

    site name change

--- resources/views/admin/player/change_password.blade.php
+++ resources/views/admin/player/change_password.blade.php
@@ -1,129 +1,131 @@
 @extends('admin_layouts.app')
 @section('styles')
-<style>
-  .transparent-btn {
-    background: none;
-    border: none;
-    padding: 0;
-    outline: none;
-    cursor: pointer;
-    box-shadow: none;
-    appearance: none;
-    /* For some browsers */
-  }
+    <style>
+        .transparent-btn {
+            background: none;
+            border: none;
+            padding: 0;
+            outline: none;
+            cursor: pointer;
+            box-shadow: none;
+            appearance: none;
+            /* For some browsers */
+        }
 
 
-  .custom-form-group {
-    margin-bottom: 20px;
-  }
+        .custom-form-group {
+            margin-bottom: 20px;
+        }
 
-  .custom-form-group label {
-    display: block;
-    margin-bottom: 5px;
-    font-weight: bold;
-    color: #555;
-  }
+        .custom-form-group label {
+            display: block;
+            margin-bottom: 5px;
+            font-weight: bold;
+            color: #555;
+        }
 
-  .custom-form-group input,
-  .custom-form-group select {
-    width: 100%;
-    padding: 10px 15px;
-    border: 1px solid #e1e1e1;
-    border-radius: 5px;
-    font-size: 16px;
-    color: #333;
-  }
+        .custom-form-group input,
+        .custom-form-group select {
+            width: 100%;
+            padding: 10px 15px;
+            border: 1px solid #e1e1e1;
+            border-radius: 5px;
+            font-size: 16px;
+            color: #333;
+        }
 
-  .custom-form-group input:focus,
-  .custom-form-group select:focus {
-    border-color: #d33a9e;
-    box-shadow: 0 0 5px rgba(211, 58, 158, 0.5);
-  }
+        .custom-form-group input:focus,
+        .custom-form-group select:focus {
+            border-color: #d33a9e;
+            box-shadow: 0 0 5px rgba(211, 58, 158, 0.5);
+        }
 
-  .submit-btn {
-    background-color: #d33a9e;
-    color: white;
-    border: none;
-    padding: 12px 20px;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 18px;
-    font-weight: bold;
-  }
+        .submit-btn {
+            background-color: #d33a9e;
+            color: white;
+            border: none;
+            padding: 12px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 18px;
+            font-weight: bold;
+        }
 
-  .submit-btn:hover {
-    background-color: #b8328b;
-  }
-</style>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.min.css">
+        .submit-btn:hover {
+            background-color: #b8328b;
+        }
+    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.min.css">
 @endsection
 @section('content')
-<div class="container text-center mt-4">
-  <div class="row">
-    <div class="col-12 col-md-8 mx-auto">
-      <div class="card">
-        <!-- Card header -->
-        <div class="card-header pb-0">
-          <div class="d-lg-flex">
-            <div>
-              <h5 class="mb-0">Change Password</h5>
+    <div class="container text-center mt-4">
+        <div class="row">
+            <div class="col-12 col-md-8 mx-auto">
+                <div class="card">
+                    <!-- Card header -->
+                    <div class="card-header pb-0">
+                        <div class="d-lg-flex">
+                            <div>
+                                <h5 class="mb-0">Change Password</h5>
 
+                            </div>
+                            <div class="ms-auto my-auto mt-lg-0 mt-4">
+                                <div class="ms-auto my-auto">
+                                    <a class="btn btn-icon btn-2 btn-primary" href="{{ route('admin.player.index') }}">
+                                        <span class="btn-inner--icon mt-1"><i
+                                                class="material-icons">arrow_back</i>Back</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form role="form" method="POST" class="text-start"
+                            action="{{ route('admin.player.makeChangePassword', $player->id) }}">
+                            @csrf
+                            <div class="custom-form-group">
+                                <label for="title">New Password <span class="text-danger">*</span></label>
+                                <input type="text" name="password" class="form-control">
+                                @error('password')
+                                    <span class="text-danger d-block">*{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="custom-form-group">
+                                <label for="title">Confirm Password <span class="text-danger">*</span></label>
+                                <input type="text" name="password_confirmation" class="form-control">
+                            </div>
+
+                            <div class="custom-form-group">
+                                <button type="submit" class="btn btn-primary" type="button">Confirm</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="ms-auto my-auto mt-lg-0 mt-4">
-              <div class="ms-auto my-auto">
-                <a class="btn btn-icon btn-2 btn-primary" href="{{ route('admin.player.index') }}">
-                  <span class="btn-inner--icon mt-1"><i class="material-icons">arrow_back</i>Back</span>
-                </a>
-              </div>
-            </div>
-          </div>
         </div>
-        <div class="card-body">
-          <form role="form" method="POST" class="text-start" action="{{ route('admin.player.makeChangePassword',$player->id) }}">
-            @csrf
-            <div class="custom-form-group">
-              <label for="title">New Password <span class="text-danger">*</span></label>
-              <input type="text"  name="password" class="form-control">
-              @error('password')
-              <span class="text-danger d-block">*{{ $message }}</span>
-              @enderror
-            </div>
-            <div class="custom-form-group">
-              <label for="title">Confirm Password <span class="text-danger">*</span></label>
-              <input type="text"  name="password_confirmation" class="form-control" >
-            </div>
-           
-            <div class="custom-form-group">
-              <button type="submit" class="btn btn-primary" type="button">Confirm</button>
-            </div>
-          </form>
-        </div>
-      </div>
     </div>
-  </div>
-</div>
 @endsection
 @section('scripts')
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.all.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.all.min.js"></script>
 
-<script src="{{ asset('admin_app/assets/js/plugins/choices.min.js') }}"></script>
-<script src="{{ asset('admin_app/assets/js/plugins/quill.min.js') }}"></script>
+    <script src="{{ asset('admin_app/assets/js/plugins/choices.min.js') }}"></script>
+    <script src="{{ asset('admin_app/assets/js/plugins/quill.min.js') }}"></script>
 
-<script>
-  var errorMessage = @json(session('error'));
-  var successMessage = @json(session('success'));
-  var url = 'https://superbet7.pro/login';
-  var name = @json(session('username'));
-  var pw = @json(session('password'));
+    <script>
+        var errorMessage = @json(session('error'));
+        var successMessage = @json(session('success'));
+        var url = 'https://betting-king-mocha.vercel.app/login';
+        var name = @json(session('username'));
+        var pw = @json(session('password'));
 
-  @if(session() -> has('success'))
-  Swal.fire({
-    title: successMessage,
-    icon: "success",
-    showConfirmButton: false,
-    showCloseButton: true,
-    html: `
+        @if (session()->has('success'))
+            Swal.fire({
+                title: successMessage,
+                icon: "success",
+                showConfirmButton: false,
+                showCloseButton: true,
+                html: `
   <table class="table table-bordered" style="background:#eee;">
   <tbody>
   <tr>
@@ -145,22 +147,23 @@
  </tbody>
   </table>
   `
-  });
-  @elseif(session()->has('error'))
-  Swal.fire({
-    icon: 'error',
-    title: errorMessage,
-    showConfirmButton: false,
-    timer: 1500
-  })
-  @endif
-  function copy() {
-       var username= $('#tusername').text();
-        var password= $('#tpassword').text();
-        var copy = "url : "+url+"\nusername : "+username+"\npw : "+password;
-        copyToClipboard(copy)
-  }
-  function copyToClipboard(v) {
+            });
+        @elseif (session()->has('error'))
+            Swal.fire({
+                icon: 'error',
+                title: errorMessage,
+                showConfirmButton: false,
+                timer: 1500
+            })
+        @endif
+        function copy() {
+            var username = $('#tusername').text();
+            var password = $('#tpassword').text();
+            var copy = "url : " + url + "\nusername : " + username + "\npw : " + password;
+            copyToClipboard(copy)
+        }
+
+        function copyToClipboard(v) {
             var $temp = $("<textarea>");
             $("body").append($temp);
             var html = v;
@@ -168,6 +171,5 @@
             document.execCommand("copy");
             $temp.remove();
         }
-
-  </script>
+    </script>
 @endsection
--- resources/views/admin_layouts/footer.blade.php
+++ resources/views/admin_layouts/footer.blade.php
@@ -1,14 +1,16 @@
 <footer class="footer py-4">
-        <div class="container-fluid">
-          <div class="row align-items-center justify-content-lg-between">
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
             <div class="col-lg-6 mb-lg-0 mb-4">
-              <div class="copyright text-center text-sm text-muted text-lg-start">
-                ©Copyright <script>
-                  document.write(new Date().getFullYear())
-                </script>,
-                by
-                <a href="https://maxwinmyanmar.com" target="_blank" class="font-weight-bold" target="_blank">SuperBet Slot</a>
-              </div>
+                <div class="copyright text-center text-sm text-muted text-lg-start">
+                    ©Copyright
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>,
+                    by
+                    <a href="https://maxwinmyanmar.com" target="_blank" class="font-weight-bold" target="_blank">BettingKing
+                        Slot</a>
+                </div>
             </div>
             {{-- <div class="col-lg-6">
               <ul class="nav nav-footer justify-content-center justify-content-lg-end">
@@ -26,6 +28,6 @@
                 </li>
               </ul>
             </div> --}}
-          </div>
         </div>
-      </footer>
+    </div>
+</footer>
